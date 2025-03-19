5ef5750409c27f7ba2e9fea19c685ec2afd82b40
Arthur Felipe <arthur.joaquim754@gmail.com>
2025-03-19 19:30:54 -0300

    Revert "Limite 20"
    This reverts commit 6f2f743258354bae32096c7bb5daf986d4e0721a.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let numerosGerados = []; // Array para armazenar os números já sorteados
-let limiteNumero = 20; // Limite superior do número secreto
+let limiteNumero = 30; // Limite superior do número secreto
 let numeroSecreto = gerarNumeroAleatorio(); // Número secreto inicial
 let totalTentativas = 1; // Contador de tentativas
 
@@ -32,7 +32,7 @@ function atualizarTexto(seletor, texto){
 // Função para exibir a mensagem inicial no jogo
 function exibirMensagemInicial(){
     atualizarTexto("h1", "🤔 Número Secreto 🤔");
-    atualizarTexto("p", "Escolha um número entre 1 e 20");
+    atualizarTexto("p", "Escolha um número entre 1 e 30");
 }
 
 exibirMensagemInicial(); // Exibe a mensagem inicial ao carregar a página
